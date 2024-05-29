ea3cb0c216f9612340c39968c4fd42aefbcf3806
Marcos Apodaca <marcosapodaca@outlook.es>
2024-05-29 13:42:01 +0200

    finish module 09.2

--- 10-Asincronía/src/main.ts
+++ 10-Asincronía/src/main.ts
@@ -1,53 +1,74 @@
 import { getDateApi } from "./fetchData";
 import { Personajes } from "./model";
 
-const contenedor = document.createElement("div");
-contenedor.className = "personaje-contenedor";
 
-
-const crearImagenPersonajes = (portada: string,): HTMLImageElement => {
+const crearImagenPersonajes = (personajes: string, apodo: string):HTMLImageElement => {
+    
     const imagen = document.createElement("img");
-    imagen.src = portada;
-    return imagen;
-};
-   
-// const crearImagenPersonajes = (personajes: string) => {
+    imagen.classList.add('personajes_img')
+    imagen.src = `http://localhost:3000/${personajes}`;
+    imagen.alt = apodo;
     
-//     const imagen = document.createElement("img");
-//     imagen.src = `http://localhost:3000/${personajes.imagen}`;
-//     imagen.alt = personajes.apodo;
-    
-//     contenedor.appendChild(imagen);
-// }
+    return imagen
+}
 
-// const crearTextoPersonajes = (personajes:string) => {
-//     const h2 = document.createElement('h2')
-//     h2.textContent = personajes.nombre
-// }
+const crearTextoPersonajes = (nombre:string, especialidad:string, habilidades:string[]):HTMLParagraphElement => {
+    const h2 = document.createElement('h2')
+    h2.classList.add("title")
+    h2.textContent = `Nombre: ${nombre}`
+
+    const testoEspecialidad = document.createElement('p')
+    testoEspecialidad.textContent = `Especialidad: ${especialidad}`
+
+    const textoHabilidad = document.createElement('p')
+    textoHabilidad.textContent = `Especialidad: ${habilidades}`
+
+    const div = document.createElement('div')
+    div.appendChild(h2)
+    div.appendChild(testoEspecialidad)
+    div.appendChild(textoHabilidad)
+
+    return div
+}
 
 const crearContenedorPersonajes = (personajes: Personajes): HTMLDivElement => {
     
-    const elementoPelicula = document.createElement("div");
-    elementoPelicula.classList.add("pelicula-contenedor");
+    const elementoPersonajes = document.createElement("div");
+    elementoPersonajes.classList.add("personaje-contenedor");
 
-    const imagen = crearImagenPersonajes(personajes.imagen)
-    elementoPelicula.appendChild(imagen)
+    const imagen = crearImagenPersonajes(personajes.imagen, personajes.apodo)
+    elementoPersonajes.appendChild(imagen)
 
-    return contenedor;
+    const nombre = crearTextoPersonajes(personajes.nombre, personajes.especialidad, personajes.habilidades)
+    elementoPersonajes.appendChild(nombre)
+
+    return elementoPersonajes;
 }
 
-const pintarPeliculas = async (): Promise<void> => {
-    console.log( await getDateApi());
-    const peliculas = await getDateApi();
-    const listado = document.querySelector("#listado-personajes");
-    if (listado && listado instanceof HTMLDivElement) {
-        peliculas.forEach((personajes:any) => {
-            const contenedorPelicula = crearContenedorPersonajes(personajes);
-            listado.appendChild(contenedorPelicula);
+
+const pintraPersonajes = async (): Promise<void> => {
+    const personajes = await getDateApi();
+    const listado:any = document.querySelector("#listado-personajes");
+    const inputBusqueda = document.querySelector("#input-busqueda") as HTMLInputElement;
+
+    const filtrarPersonajes = () => {
+        const filtro = inputBusqueda.value.toLowerCase();
+        const personajesFiltrados = personajes.filter((personaje: any) => {
+            return personaje.nombre.toLowerCase().includes(filtro);
         });
-    } else {
-        throw new Error("No se ha encontrado el contenedor del listado");
-    }
+
+        listado.innerHTML = '';
+
+        personajesFiltrados.forEach((personaje: any) => {
+            const contenedorPersonajes = crearContenedorPersonajes(personaje);
+            listado.appendChild(contenedorPersonajes);
+        });
+    };
+
+    inputBusqueda.addEventListener("input", filtrarPersonajes);
+
+    filtrarPersonajes();
 };
 
-document.addEventListener("DOMContentLoaded", pintarPeliculas);
+document.addEventListener("DOMContentLoaded", pintraPersonajes);
+
