23c9403b1fa42753fd5390e98bc32a6282765868
Marcos Apodaca <marcosapodaca@outlook.es>
2024-11-14 23:39:07 +0100

    change classe

--- 12-clases/src/main.ts
+++ 12-clases/src/main.ts
@@ -1,31 +1,5 @@
-interface Reserva {
-    tipoHabitacion: "standard" | "suite";
-    pax: number;
-    noches: number;
-  }
-  
-  const reservas = [
-    {
-      tipoHabitacion: "standard",
-      pax: 1,
-      noches: 3,
-    },
-    {
-      tipoHabitacion: "standard",
-      pax: 1,
-      noches: 4,
-    },
-    {
-      tipoHabitacion: "suite",
-      pax: 2,
-      noches: 1,
-    },
-  ];
-
-
-//   interface Reserva {
+// interface Reserva {
 //     tipoHabitacion: "standard" | "suite";
-//     desayuno: boolean;
 //     pax: number;
 //     noches: number;
 //   }
@@ -33,20 +7,19 @@ interface Reserva {
 //   const reservas = [
 //     {
 //       tipoHabitacion: "standard",
-//       desayuno: false,
 //       pax: 1,
 //       noches: 3,
 //     },
 //     {
 //       tipoHabitacion: "standard",
-//       desayuno: false,
 //       pax: 1,
 //       noches: 4,
 //     },
 //     {
 //       tipoHabitacion: "suite",
-//       desayuno: true,
 //       pax: 2,
 //       noches: 1,
 //     },
 //   ];
+
+
